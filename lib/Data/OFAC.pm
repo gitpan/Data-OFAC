a4c15e82623a30717036988c02d1349a95bf9377
Tyler Hardison <tyler@seraph-net.net>
2013-11-14 20:35:11 +0000

    Import of THARDISON/Data-OFAC-0.52 from CPAN.
    gitpan-cpan-distribution: Data-OFAC
    gitpan-cpan-version:      0.52
    gitpan-cpan-path:         THARDISON/Data-OFAC-0.52.tar.gz
    gitpan-cpan-author:       THARDISON
    gitpan-cpan-maturity:     released

--- lib/Data/OFAC.pm
+++ lib/Data/OFAC.pm
@@ -12,11 +12,11 @@ Control (OFAC) Specially Designated Nationals List (SDN)
 
 =head1 VERSION
 
-Version 0.51
+Version 0.52
 
 =cut
 
-our $VERSION = '0.51';
+our $VERSION = '0.52';
 
 =head1 DESCRIPTION
 
